93794b71d95bda168951684e58e954fa50029a4a
ssli <382715246@qq.com>
2022-10-14 13:59:12 +0800

    fix setup

--- src/quafu/circuits/quantum_circuit.py
+++ src/quafu/circuits/quantum_circuit.py
@@ -363,7 +363,7 @@ class QuantumCircuit(object):
         Returns: 
             openqasm text.
         """
-        qasm = '''OPENQASM 2.0;\ninclude "qelib1.inc";\n'''
+        qasm = "OPENQASM 2.0;\ninclude \"qelib1.inc\";\n"
         qasm += "qreg q[%d];\n" % self.num
         qasm += "creg meas[%d];\n" % len(self.measures)
         for gate in self.gates:
@@ -616,6 +616,7 @@ class QuantumCircuit(object):
             tar (int): target qubit.
         """
         self.gates.append(CSGate([ctrl, tar]))
+        return self
 
     def ct(self, ctrl: int, tar: int) -> "QuantumCircuit":
         """
@@ -626,6 +627,7 @@ class QuantumCircuit(object):
         """
         
         self.gates.append(CTGate([ctrl, tar]))
+        return self
 
     def cp(self, ctrl: int, tar: int, para) -> "QuantumCircuit":
         """
@@ -636,7 +638,7 @@ class QuantumCircuit(object):
             tar (int): target qubit.
         """
         self.gates.append(CPGate([ctrl, tar], para))
-
+        return self
     # def fsim(self, q1, q2, theta, phi):
     #     """
     #     fSim gate.
