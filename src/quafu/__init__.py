53cf8c53e7b1dc9987eeb04e8310124c87a2fd47
ssli <382715246@qq.com>
2022-10-20 12:28:51 +0800

    update version

--- src/quafu/__init__.py
+++ src/quafu/__init__.py
@@ -7,5 +7,5 @@ from .simulators.default_simulator import simulate
 __all__ = ["QuantumCircuit", "ExecResult", "Task", "User", "SimuResult", "simulate"]
 
 def get_version():
-    print ("version: 0.2.6\n")
-    return "0.2.6"
+    print ("version: 0.2.7\n")
+    return "0.2.7"
