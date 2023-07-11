ec56ecbb80ea64886285b6cc46731de779b40f3c
chensgit169 <weichen191@mails.ucas.ac.cn>
2023-07-11 13:19:01 +0800

    "Add Superclass Instruction"
    And set a class attribute 'name' for the subclasses.

--- src/quafu/elements/quantum_element.py
+++ src/quafu/elements/quantum_element.py
@@ -1,7 +1,29 @@
-# This is the file for abstract quantum gates class
+#  (C) Copyright 2023 Beijing Academy of Quantum Information Sciences
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from abc import ABC
 from typing import Union, Callable, List, Iterable
 
 import numpy as np
+
+"""
+Base classes for ALL kinds of possible instructions on superconducting 
+quantum circuits.
+"""
+
+ParaType = dict[str: float | int]
+PosType = Union[int, List[int]]
 
 
 def reorder_matrix(matrix: np.ndarray, pos: List):
@@ -14,9 +36,80 @@ def reorder_matrix(matrix: np.ndarray, pos: List):
     return np.transpose(tensorm, inds).reshape([dim, dim])
 
 
-class Barrier(object):
+class Instruction(ABC):
+    name = None  # type: str
+    # """
+    # Metaclass for primitive instructions in DIRECTED ACYCLIC quantum circuits
+    # or quantum-classical circuits.
+    #
+    # Member variables:
+    #
+    # - sd_name: standard name without args or extra annotations. Used for communications
+    #   and identifications within pyquafu program, for example, to translate a qu_gate into qasm.
+    #   Ideally every known primitive during computation should have such a name, and should be
+    #   chosen as the most commonly accepted convention. NOT allowed to be changed by users once
+    #   the class is instantiated.
+    #
+    # - pos: positions of relevant quantum or classical bits, legs of DAG.
+    #
+    # - dorder: depth order, or topological order of DAG
+    #
+    # - symbol: label that can be freely customized by users. If sd_name is not None, name is the
+    #   same as sd_name by default. Otherwise, a symbol has to be specified while sd_name remains
+    #   as None to indicate that this is a use-defined class.
+    # """
+    #
+    # _ins_id = None  # type: str
+    #
+    # def __init__(self, pos: PosType, label: str = None, paras: ParaType = None):
+    #     # if pos is not iterable, make it be
+    #     self.pos = [pos] if isinstance(pos, int) else pos
+    #     if label:
+    #         self.label = label
+    #     else:
+    #         if self._ins_id is None:
+    #             raise ValueError('For user-defined instruction, label has to be specified.')
+    #         self.label = self._ins_id
+    #         if paras:
+    #             self.label += '(' + ', '.join(['%.3f' % _ for _ in paras.values()]) + ')'
+    #     self.paras = paras
+    #
+    # @classmethod
+    # def get_ins_id(cls):
+    #     return cls._ins_id
+    #
+    # @abstractmethod
+    # def openqasm2(self) -> str:
+    #     pass
+
+    # @_ins_id.setter
+    # def sd_name(self, name: str):
+    #     if self.sd_name is None:
+    #         self.sd_name = name
+    #     else:
+    #         import warnings
+    #         warnings.warn(message='Invalid assignment, names of standard '
+    #                               'instructions are not alterable.')
+
+    # def to_dag_node(self):
+    #     name = self.get_ins_id()
+    #     label = self.__repr__()
+    #
+    #     pos = self.pos
+    #     paras = self.paras
+    #     paras = {} if paras is None else paras
+    #     duration = paras.get('duration', None)
+    #     unit = paras.get('unit', None)
+    #     channel = paras.get('channel', None)
+    #     time_func = paras.get('time_func', None)
+    #
+    #     return InstructionNode(name, pos, paras, duration, unit, channel, time_func, label)
+
+
+class Barrier(Instruction):
+    name = "barrier"
+
     def __init__(self, pos):
-        self.name = "barrier"
         self.__pos = pos
         self.symbol = "||"
 
@@ -35,9 +128,10 @@ class Barrier(object):
         return "barrier " + ",".join(["q[%d]" % p for p in range(min(self.pos), max(self.pos) + 1)])
 
 
-class Delay(object):
+class Delay(Instruction):
+    name = "delay"
+
     def __init__(self, pos: int, duration: int, unit="ns"):
-        self.name = "delay"
         if isinstance(duration, int):
             self.duration = duration
         else:
@@ -53,9 +147,10 @@ class Delay(object):
         return "delay(%d%s) q[%d]" % (self.duration, self.unit, self.pos)
 
 
-class XYResonance(object):
+class XYResonance(Instruction):
+    name = "XY"
+
     def __init__(self, qs: int, qe: int, duration: int, unit="ns"):
-        self.name = "XY"
         if isinstance(duration, int):
             self.duration = duration
         else:
@@ -69,15 +164,21 @@ class XYResonance(object):
             ["q[%d]" % p for p in range(min(self.pos), max(self.pos) + 1)])
 
 
-class Measure(object):
+class Measure(Instruction):
+    name = "measure"
+
     def __init__(self, bitmap: dict):
         self.qbits = bitmap.keys()
         self.cbits = bitmap.values()
 
 
-class QuantumGate(object):
-    def __init__(self, name: str, pos: Union[int, List[int]], paras: Union[None, float, List[float]], matrix):
-        self.name = name
+class QuantumGate(Instruction, ABC):
+    name = 'gate'
+
+    def __init__(self,
+                 pos: PosType,
+                 paras: Union[float, List[float]] = None,
+                 matrix=None):
         self.pos = pos
         self.paras = paras
         self.matrix = matrix
@@ -89,14 +190,6 @@ class QuantumGate(object):
                 self.symbol = "%s(%.3f)" % (self.name, paras)
         else:
             self.symbol = "%s" % self.name
-
-    @property
-    def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, _name):
-        self.__name = _name
 
     @property
     def pos(self):
@@ -149,8 +242,10 @@ class QuantumGate(object):
 
 
 class SingleQubitGate(QuantumGate):
-    def __init__(self, name: str, pos: int, paras, matrix):
-        super().__init__(name, pos, paras=paras, matrix=matrix)
+    name = 'su(2)'
+
+    def __init__(self, pos: int, paras, matrix):
+        super().__init__(pos, paras=paras, matrix=matrix)
 
     @property
     def matrix(self):
@@ -173,13 +268,13 @@ class SingleQubitGate(QuantumGate):
 
 
 class FixedSingleQubitGate(SingleQubitGate):
-    def __init__(self, name, pos, matrix):
-        super().__init__(name, pos, paras=None, matrix=matrix)
+    def __init__(self, pos, matrix):
+        super().__init__(pos, paras=None, matrix=matrix)
 
 
 class ParaSingleQubitGate(SingleQubitGate):
-    def __init__(self, name, pos, paras, matrix):
-        super().__init__(name, pos, paras=paras, matrix=matrix)
+    def __init__(self, pos, paras, matrix):
+        super().__init__(pos, paras=paras, matrix=matrix)
 
     @property
     def matrix(self):
@@ -201,8 +296,10 @@ class ParaSingleQubitGate(SingleQubitGate):
 
 
 class MultiQubitGate(QuantumGate):
-    def __init__(self, name: str, pos: List[int], paras, matrix):
-        super().__init__(name, pos, paras=paras, matrix=matrix)
+    name = 'su(n)'
+
+    def __init__(self, pos: List[int], paras, matrix):
+        super().__init__(pos, paras=paras, matrix=matrix)
         self._targ_matrix = matrix
 
     @property
@@ -234,13 +331,13 @@ class MultiQubitGate(QuantumGate):
 
 
 class FixedMultiQubitGate(MultiQubitGate):
-    def __init__(self, name: str, pos: List[int], matrix):
-        super().__init__(name, pos, paras=None, matrix=matrix)
+    def __init__(self, pos: List[int], matrix):
+        super().__init__(pos, paras=None, matrix=matrix)
 
 
 class ParaMultiQubitGate(MultiQubitGate):
-    def __init__(self, name, pos, paras, matrix):
-        super().__init__(name, pos, paras, matrix=matrix)
+    def __init__(self, pos, paras, matrix):
+        super().__init__(pos, paras, matrix=matrix)
 
     @property
     def matrix(self):
@@ -261,11 +358,11 @@ class ParaMultiQubitGate(MultiQubitGate):
 class ControlledGate(MultiQubitGate):
     """ Controlled gate class, where the matrix act non-trivaly on target qubits"""
 
-    def __init__(self, name, targe_name, ctrls: List[int], targs: List[int], paras, matrix):
+    def __init__(self, targe_name, ctrls: List[int], targs: List[int], paras, matrix):
         self.ctrls = ctrls
         self.targs = targs
         self.targ_name = targe_name
-        super().__init__(name, ctrls + targs, paras, matrix)
+        super().__init__(ctrls + targs, paras, matrix)
         self._targ_matrix = matrix
 
         if paras:
@@ -314,13 +411,17 @@ class ControlledGate(MultiQubitGate):
 
 
 class ControlledU(ControlledGate):
-    def __init__(self, name, ctrls: List[int], U: Union[SingleQubitGate, MultiQubitGate]):
+    """ Controlled gate class, where the matrix act non-trivaly on target qubits"""
+
+    name = 'cu'
+
+    def __init__(self, ctrls: List[int], U: Union[SingleQubitGate, MultiQubitGate]):
         self.targ_gate = U
         targs = U.pos
         if isinstance(targs, int):
             targs = [targs]
 
-        super().__init__(name, U.name, ctrls, targs, U.paras, matrix=self.targ_gate.get_targ_matrix())
+        super().__init__(U.name, ctrls, targs, U.paras, matrix=self.targ_gate.get_targ_matrix())
 
     def get_targ_matrix(self, reverse_order=False):
         return self.targ_gate.get_targ_matrix(reverse_order)
