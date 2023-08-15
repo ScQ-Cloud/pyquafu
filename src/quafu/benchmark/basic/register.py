88b76f66165f5801c7769effc0b86245445c6504
chensgit169 <weichen191@mails.ucas.ac.cn>
2023-08-15 15:38:41 +0800

    Revert "merge from my-master branch"
    This reverts commit f5cdf86a81a49eff292da3b36fcf7470a62b4410.

--- src/quafu/benchmark/basic/register.py
+++
@@ -1,5 +0,0 @@
-from quafu.users.userapi import User
-
-user = User(api_token='wuNZGTDAHQfsQ0Ngtq_j0fh1RLV-IeeM7eFWJeN1-21.9dDN0QzMyQTO2EjOiAHelJCL3k'
-                      'DN6ICZpJye.9JiN1IzUIJiOicGbhJCLiQ1VKJiOiAXe0Jye')
-user.save_apitoken()
