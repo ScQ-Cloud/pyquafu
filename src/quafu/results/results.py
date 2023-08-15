1fed1a65f2ac19081fb54fc200a2dcabeecfc9c7
chensgit169 <weichen191@mails.ucas.ac.cn>
2023-08-15 19:22:24 +0800

    fix measures 'NoneType' problem in retrieve
    cancel 'measures' as argument if init ExecResult, for it could be extracted directly from transpiled_circuit.

--- src/quafu/results/results.py
+++ src/quafu/results/results.py
@@ -21,18 +21,8 @@ class ExecResult(Result):
         transpiled_circuit (QuantumCircuit): Quantum circuit transpiled on backend.
     """
 
-    def __init__(self, input_dict, measures):
+    def __init__(self, input_dict):
         status_map = {0: "In Queue", 1: "Running", 2: "Completed", "Canceled": 3, 4: "Failed"}
-        self.measures = measures
-        self.task_status = status_map[input_dict["status"]]
-        self.res = eval(input_dict['res'])
-        self.counts = OrderedDict(sorted(self.res.items(), key=lambda s: s[0]))
-        self.logicalq_res = {}
-        cbits = list(self.measures.values())
-        for key, values in self.counts.items():
-            newkey = "".join([key[i] for i in cbits])
-            self.logicalq_res[newkey] = values
-
         self.taskid = input_dict['task_id']
         self.taskname = input_dict['task_name']
         self.transpiled_openqasm = input_dict["openqasm"]
@@ -40,10 +30,21 @@ class ExecResult(Result):
         self.transpiled_circuit = QuantumCircuit(0)
         self.transpiled_circuit.from_openqasm(self.transpiled_openqasm)
         self.measure_base = []
+
+        self.measures = self.transpiled_circuit.measures
+        self.task_status = status_map[input_dict["status"]]
+        self.res = eval(input_dict['res'])
+
+        self.counts = OrderedDict(sorted(self.res.items(), key=lambda s: s[0]))
+        self.logicalq_res = {}
+        for bit_str, count in self.counts.items():
+            newkey = "".join([bit_str[i] for i in self.measures.values()])
+            self.logicalq_res[newkey] = count
+
         total_counts = sum(self.counts.values())
         self.probabilities = {}
-        for key in self.counts:
-            self.probabilities[key] = self.counts[key] / total_counts
+        for bit_str in self.counts:
+            self.probabilities[bit_str] = self.counts[bit_str] / total_counts
 
     def calculate_obs(self, pos):
         """
