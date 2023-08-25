f0b9877e75eebd86fbfe0c40647328af5b9a9538
Zhaoyilunnn <dilved1014@gmail.com>
2023-08-25 09:01:02 +0000

    minor: add quantum_register classes in quafu `__init__.py`

--- src/quafu/__init__.py
+++ src/quafu/__init__.py
@@ -1,4 +1,5 @@
 from .circuits.quantum_circuit import QuantumCircuit
+from .circuits.quantum_register import QuantumRegister, Qubit
 from .results.results import ExecResult, SimuResult
 from .tasks.tasks import Task
 from .users.userapi import User
@@ -6,6 +7,8 @@ from .simulators.simulator import simulate
 
 __all__ = [
     "QuantumCircuit",
+    "QuantumRegister",
+    "Qubit",
     "ExecResult",
     "Task",
     "User",
