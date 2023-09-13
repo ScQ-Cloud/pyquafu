1a3046405c878cc8fc28dd98170fcca3f72553ab
chensgit169 <weichen191@mails.ucas.ac.cn>
2023-09-13 10:02:41 +0800

    move function of draw_dag() from ``dagcircuit`` into ``visualization``
    Decouple from IPython, pygraphviz

--- quafu/visualisation/draw_dag.py
+++ quafu/visualisation/draw_dag.py
@@ -7,7 +7,7 @@ from quafu.dagcircuits.instruction_node import InstructionNode
 
 def _extract_node_info(node):
     if isinstance(node, InstructionNode):
-        name, label = str(id(node)), node.name
+        name, label = str(id(node)), str(node)
     else:
         assert node == -1 or node == float("inf")
         name, label = str(node), str(node)
@@ -15,6 +15,40 @@ def _extract_node_info(node):
 
 
 def draw_dag(qc: QuantumCircuit):
+    """
+        Helper function to visualize the DAG
+
+        Args:
+            dep_g (DAG): DAG with Hashable Gates
+            output_format (str): output format, "png" or "svg"
+
+        Returns:
+            img (Image or SVG): show the image of DAG, which is Image(filename="dag.png") or SVG(filename="dag.svg")
+
+        example:
+            .. jupyter-execute::
+            ex1:
+                # directly draw  PNG picture
+                draw_dag(dep_g, output_format="png")    # save a png picture "dag.png" and show it in jupyter notebook
+
+                # directly draw  SVG   picture
+                draw_dag(dep_g, output_format="svg")    # save a svg picture "dag.svg" and show it in jupyter notebook
+
+            ex2:
+                # generate   PNG  picture
+                img_png = draw_dag(dep_g, output_format="png")
+
+                # generate   SVG  picture
+                img_svg = draw_dag(dep_g, output_format="svg")
+
+                # show PNG picture
+                img_png
+
+                # show SVG picture
+                img_svg
+
+
+        """
     dag = circuit_to_dag(qc)
     dot = graphviz.Digraph()
 
