3cd63e35785630a71b68d6d42825a22e684c81ab
chensgit169 <weichen191@mails.ucas.ac.cn>
2023-10-16 12:09:41 +0800

    unify interface of quantum_gates, remove some duplicates

--- src/quafu/elements/element_gates/__init__.py
+++ src/quafu/elements/element_gates/__init__.py
@@ -4,17 +4,14 @@ from .phase import PhaseGate
 from .rotation import RXGate, RYGate, RZGate, RXXGate, RYYGate, RZZGate
 from .swap import SwapGate, ISwapGate
 from .c11 import CXGate, CYGate, CZGate, CSGate, CTGate, CPGate
-from .c21 import ToffoliGate
 from .c12 import FredkinGate
-from .cm1 import MCXGate, MCYGate, MCZGate, ControlledU
+from .cm1 import MCXGate, MCYGate, MCZGate, ToffoliGate
 from .unitary import UnitaryDecomposer
 
 __all__ = ['XGate', 'YGate', 'ZGate', 'IdGate', 'WGate', 'SWGate',
            'PhaseGate',
            'RXGate', 'RYGate', 'RZGate', 'RXXGate', 'RYYGate', 'RZZGate',
-           'SwapGate', 'ISwapGate',
+           'SwapGate', 'ISwapGate', 'FredkinGate',
            'CXGate', 'CYGate', 'CZGate', 'CSGate', 'CTGate', 'CPGate',
-           'ToffoliGate',
-           'FredkinGate',
-           'MCXGate', 'MCYGate', 'MCZGate', 'ControlledU',
+           'MCXGate', 'MCYGate', 'MCZGate', 'ToffoliGate',
            'UnitaryDecomposer']
--- src/quafu/elements/element_gates/c11.py
+++ src/quafu/elements/element_gates/c11.py
@@ -1,49 +1,55 @@
-from ..quantum_element import ControlledGate
+from ..quantum_gate import ControlledGate, FixedGate
 from abc import ABC
-from .matrices import XMatrix, YMatrix, ZMatrix, SMatrix, TMatrix, pmatrix
+from quafu.elements.matrices import XMatrix, YMatrix, ZMatrix, SMatrix, TMatrix, pmatrix
+from typing import Dict
+
+
+__all__ = ['CXGate', 'CYGate', 'CZGate',
+           'CSGate', 'CTGate',
+           'CPGate']
 
 
 class _C11Gate(ControlledGate, ABC):
     ct_dims = (1, 1, 2)
 
 
-class CXGate(_C11Gate):
+class CXGate(_C11Gate, FixedGate):
     name = "CX"
 
     def __init__(self, ctrl: int, targ: int):
-        super().__init__("X", [ctrl], [targ], None, tar_matrix=XMatrix)
+        _C11Gate.__init__(self, "X", [ctrl], [targ], None, tar_matrix=XMatrix)
         self.symbol = "+"
 
 
-class CYGate(_C11Gate):
+class CYGate(_C11Gate, FixedGate):
     name = "CY"
 
     def __init__(self, ctrl: int, targ: int):
-        super().__init__("Y", [ctrl], [targ], None, tar_matrix=YMatrix)
+        _C11Gate.__init__(self, "Y", [ctrl], [targ], None, tar_matrix=YMatrix)
 
 
-class CZGate(_C11Gate):
+class CZGate(_C11Gate, FixedGate):
     name = "CZ"
 
     def __init__(self, ctrl: int, targ: int):
-        super().__init__("Z", [ctrl], [targ], None, tar_matrix=ZMatrix)
+        _C11Gate.__init__(self, "Z", [ctrl], [targ], None, tar_matrix=ZMatrix)
 
 
-class CSGate(_C11Gate):
+class CSGate(_C11Gate, FixedGate):
     name = "CS"
 
     def __init__(self, ctrl: int, targ: int):
-        super().__init__("S", [ctrl], [targ], None, tar_matrix=SMatrix)
+        _C11Gate.__init__(self, "S", [ctrl], [targ], None, tar_matrix=SMatrix)
 
     def to_qasm(self):
         return "cp(pi/2) " + "q[%d],q[%d]" % (self.pos[0], self.pos[1])
 
 
-class CTGate(_C11Gate):
+class CTGate(_C11Gate, FixedGate):
     name = "CT"
 
     def __init__(self, ctrl: int, targ: int):
-        super().__init__("T", [ctrl], [targ], None, tar_matrix=TMatrix)
+        _C11Gate.__init__(self, "T", [ctrl], [targ], None, tar_matrix=TMatrix)
 
     def to_qasm(self):
         return "cp(pi/4) " + "q[%d],q[%d]" % (self.pos[0], self.pos[1])
@@ -53,7 +59,11 @@ class CPGate(_C11Gate):
     name = "CP"
 
     def __init__(self, ctrl: int, targ: int, paras):
-        super().__init__("P", [ctrl], [targ], paras, tar_matrix=pmatrix(paras))
+        _C11Gate.__init__(self, "P", [ctrl], [targ], paras, tar_matrix=pmatrix(paras))
+
+    @property
+    def named_paras(self) -> Dict:
+        return {'theta': self.paras}
 
 
 ControlledGate.register_gate(CXGate)
