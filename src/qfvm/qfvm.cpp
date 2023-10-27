eef696c1f72a9da5cdbb34c1a445fb27cacda666
beizha <beizhansl@163.com>
2023-10-27 20:05:41 +0800

    Decide when to make the measurement projection based on the position of the measure operator

--- src/qfvm/qfvm.cpp
+++ src/qfvm/qfvm.cpp
@@ -1,7 +1,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include "simulator.hpp"
-
+#include <iostream>
 #ifdef _USE_GPU
 #include <cuda_simulator.cuh>
 #endif
@@ -27,27 +27,77 @@ py::array_t<T> to_numpy(const std::tuple<T*, size_t> &src) {
     );
 }
 
-py::object execute(string qasm){
-    return to_numpy(simulate(qasm).move_data_to_python());
-}
-
-py::object simulate_circuit(py::object const&pycircuit, py::array_t<complex<double>> &np_inputstate){
+std::pair<std::map<uint, uint>, py::array_t<complex<double>> > simulate_circuit(py::object const&pycircuit, py::array_t<complex<double>> &np_inputstate, const int &shots){
     auto circuit = Circuit(pycircuit);
     py::buffer_info buf = np_inputstate.request();
     auto* data_ptr = reinterpret_cast<std::complex<double>*>(buf.ptr);
     size_t data_size = buf.size;
-
-    if (data_size == 0){
+    // If measure all at the end, simulate once
+    uint actual_shots = shots;
+    if (circuit.final_measure()) actual_shots = 1;
+    StateVector<double> global_state;
+    std::map<uint,uint>measures = circuit.measure_map();
+    std::map<uint,bool>cbit_measured;
+    for(auto &pair: measures){
+        cbit_measured[pair.second] = true;        
+    }
+    // Store outcome's count
+    std::map<uint, uint> outcount;
+    for(uint i =0; i < actual_shots; i++){
         StateVector<double> state;
-        simulate(circuit, state);
-        return to_numpy(state.move_data_to_python());
+        if(data_size == 0){
+            simulate(circuit, state);
+        }else{
+            //deepcopy state
+            vector<std::complex<double>> data_copy(data_ptr, data_ptr + data_size);
+            state = std::move(StateVector<double>(data_copy.data(), data_copy.size()));
+            simulate(circuit, state);
+        }
+        if (circuit.final_measure() || i == actual_shots-1) global_state = std::move(state);
+        else{
+            // store reg
+            vector<uint> tmpcreg = state.creg();
+            uint outcome = 0;
+            for(uint i=0;i<tmpcreg.size();i++){
+                if(cbit_measured.find(i) == cbit_measured.end()) continue; 
+                outcome *= 2;
+                outcome += tmpcreg[i];
+            }
+            if(outcount.find(outcome) != outcount.end()) outcount[outcome]++;
+            else outcount[outcome] = 1;
+        }
     }
-    else{
-      StateVector<double> state(data_ptr, buf.size);
-      simulate(circuit, state);
-      state.move_data_to_python();
-      return np_inputstate;
-    }
+    // sample outcome if final_measure is true
+    if(circuit.final_measure() && !measures.empty()){
+        vector<uint> tmpcount(global_state.size(), 0);
+        vector<double> probs = global_state.probabilities();
+        for(uint i = 0; i < shots; i++){
+            uint outcome = std::discrete_distribution<uint>(probs.begin(), probs.end())(global_state.rng());
+            tmpcount[outcome]++;
+        }
+        // map to reg
+        for(uint i = 0; i < global_state.size(); i++){
+            if(tmpcount[i] == 0) continue;
+            vector<uint> tmpcreg(global_state.cbit_num(), 0); 
+            vector<uint> tmpout = int2vec(i, 2);
+            if(tmpout.size() < global_state.num())
+                tmpout.resize(global_state.num());
+            for(auto &pair: measures){
+                tmpcreg[pair.second] = tmpout[pair.first];
+            }
+            uint outcome = 0;
+            for(uint i=0;i<tmpcreg.size();i++){
+                if(cbit_measured.find(i) == cbit_measured.end()) continue; 
+                outcome *= 2;
+                outcome += tmpcreg[i];
+            }
+            if(outcount.find(outcome) != outcount.end()) outcount[outcome] += tmpcount[i];
+            else outcount[outcome] = tmpcount[i];
+        }
+    } 
+    // return 
+    if(data_size == 0) return std::make_pair(outcount, to_numpy(global_state.move_data_to_python()));
+    else return std::make_pair(outcount, np_inputstate);  
 }
 
 #ifdef _USE_GPU
@@ -98,8 +148,7 @@ py::object simulate_circuit_custate(py::object const&pycircuit, py::array_t<comp
 
 PYBIND11_MODULE(qfvm, m) {
     m.doc() = "Qfvm simulator";
-    m.def("execute", &execute, "Simulate with qasm");
-    m.def("simulate_circuit", &simulate_circuit, "Simulate with circuit", py::arg("circuit"), py::arg("inputstate")= py::array_t<complex<double>>(0));
+    m.def("simulate_circuit", &simulate_circuit, "Simulate with circuit", py::arg("circuit"), py::arg("inputstate")= py::array_t<complex<double>>(0), py::arg("shots"));
 
     #ifdef _USE_GPU
      m.def("simulate_circuit_gpu", &simulate_circuit_gpu, "Simulate with circuit", py::arg("circuit"), py::arg("inputstate")= py::array_t<complex<double>>(0));
