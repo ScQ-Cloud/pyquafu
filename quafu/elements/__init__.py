c48fd463dbe6d80fcbe6575b962eb57be07755e6
chensgit169 <weichen191@mails.ucas.ac.cn>
2023-11-18 22:17:18 +0800

    fix __init__ imports, add Cif as well

--- quafu/elements/__init__.py
+++ quafu/elements/__init__.py
@@ -1,1 +1,4 @@
-from .instruction import Instruction
+from .instruction import Instruction, Barrier, Measure
+from .pulses import Delay, XYResonance, QuantumPulse
+from .quantum_gate import QuantumGate, ControlledGate, MultiQubitGate, SingleQubitGate
+from .classical_element import Cif
