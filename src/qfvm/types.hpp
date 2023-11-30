e8d371cf64538a9df2b1607b94446fd994347169
chensgit169 <weichen191@mails.ucas.ac.cn>
2023-11-30 16:09:35 +0800

    Revert "remove redundant files"
    This reverts commit 4efd9b53b12b7c0faf54462101474e8c4810a07a.

---
+++ src/qfvm/types.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <complex>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <tuple>
+#include <Eigen/Core>
+
+#ifdef _MSC_VER
+using omp_i = signed long long;
+#else
+using omp_i = size_t;
+#endif
+
+#ifdef _MSC_VER
+#else
+#ifndef __AVX2__
+#undef USE_SIMD
+#endif
+#endif
+
+
+typedef unsigned int uint;
+using pos_t = uint;
+using data_t = double;
+using std::complex;
+using std::vector;
+using std::string;
+using RowMatrixXcd = Eigen::Matrix<complex<double>, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+
+const complex<double> imag_I = complex<double>(0, 1.);
+const double PI = 3.14159265358979323846;
